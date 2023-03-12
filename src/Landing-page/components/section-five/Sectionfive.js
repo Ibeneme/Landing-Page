d1d58f2adfabf203a5405794fcee1af966e66e0d
ibeneme ikenna <ibenemeikenna96@gmail>
2023-03-12 15:58:46 +0100

    updates

--- src/Landing-page/components/section-five/Sectionfive.js
+++ src/Landing-page/components/section-five/Sectionfive.js
@@ -11,9 +11,7 @@ export const SectionFive = () => {
                 We take your security seriously
             </h1>
             <p  className='div-background-p' >
-            Get verified on any website or mobile application needing KYC in minutes — not hours — not minutes with our simple and out-of-the-box solution. Once you are verified on Passcoder, you are verified everywhere else.
-            </p>
-            <ButtonOrangered />
+            Passcoder is certified to the highest industry data privacy standards — protecting your privacy.  Our system is 100% encrypted. We only help you store your data. We do not have access to it. Your data is safe with us.   <ButtonOrangered />
         </div>
         
 
