4eaf26aafdbc1bfdad6a19736cbfd66ba1f377d1
ibeneme ikenna <ibenemeikenna96@gmail>
2023-03-12 16:04:37 +0100

    updates

--- src/Landing-page/components/section-three/SectionThree.js
+++ src/Landing-page/components/section-three/SectionThree.js
@@ -14,11 +14,7 @@ export const SectionThree = () => {
             }}> 
                 We take your security seriously
             </h1>
-            <p>Passcoder is certified to the highest industry data privacy standards 
-                — protecting you and your customers' privacy. We regularly ask your 
-                consent third-party access to your details, 
-                Our system is 100% encrypted, we store your data but we don't have access to it, we do not have access to your data, Your data is safe with us.
-                 
+            <p>Passcoder is certified to the highest industry data privacy standards — protecting your privacy.  Our system is 100% encrypted. We only help you store your data. We do not have access to it. Your data is safe with us.
             </p>
             <Buttonblue />
         </div>
